5066ef9517cb6c2d38b39b73fc4bb791cb6a7ac3
Lele Chen <lchen63@ur.rochester.edu>
2019-10-18 14:28:15 -0400

    g

--- data/dataset.py
+++ data/dataset.py
@@ -43,7 +43,7 @@ class Lmark2rgbDataset(Dataset):
         self.root  = opt.dataroot
 
         if opt.isTrain:
-            _file = open(os.path.join(self.root, 'txt',  "train_front_rt.pkl"), "rb")
+            _file = open(os.path.join(self.root, 'txt',  "train_front_rt2.pkl"), "rb")
             # self.data = pkl.load(_file)
             self.data = pkl._Unpickler(_file)
             self.data.encoding = 'latin1'
