f6cbdfdae98aba07b73d3851ba3e51096d51c075
lelechen63 <lchen63@ur.rochester.edu>
2019-10-23 22:12:14 -0400

    g

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -26,7 +26,8 @@ class CustomDatasetDataLoader(BaseDataLoader):
             self.dataset, pin_memory= True,
             batch_size=opt.batchSize,
             shuffle = not opt.serial_batches,
-            num_workers=int(opt.nThreads))
+            num_workers=int(opt.nThreads),
+             drop_last=True)
 
     def load_data(self):
         return self.dataloader
