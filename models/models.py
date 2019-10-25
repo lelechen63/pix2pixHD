c52fa3ff155c0597d0014393866912a9f05531f6
lelechen63 <lchen63@ur.rochester.edu>
2019-10-25 15:28:37 -0400

    add deform conv

--- models/models.py
+++ models/models.py
@@ -12,7 +12,7 @@ def create_model(opt):
         from .Lmark2RGB_model import Lmark2RGBModel1, InferenceModel1
         if opt.isTrain:
             model = Lmark2RGBModel1()
-    else:
+        else:
            model = InferenceModel1()
     elif opt.model == 'base2':
         from .Lmark2RGB_model import Lmark2RGBModel2, InferenceModel2
