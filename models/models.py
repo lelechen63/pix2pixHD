0481a5f46ba861468d3bb5093408759cf0f76354
lelechen63 <lchen63@ur.rochester.edu>
2019-10-25 13:00:23 -0400

    add deform conv

--- models/models.py
+++ models/models.py
@@ -1,7 +1,6 @@
 ### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
 ### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 import torch
-
 def create_model(opt):
     if opt.model == 'pix2pixHD':
         from .pix2pixHD_model import Pix2PixHDModel, InferenceModel
@@ -27,8 +26,6 @@ def create_model(opt):
     model.initialize(opt)
     if opt.verbose:
         print("model [%s] was created" % (model.name()))
-
     if opt.isTrain and len(opt.gpu_ids):
         model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids).cuda()
-
     return model
