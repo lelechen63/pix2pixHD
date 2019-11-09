4e62462c8a7737281ceebac7531ca4279e89ba2e
Lele Chen <lchen63@ur.rochester.edu>
2019-11-09 15:36:21 -0500

    update for lstm

--- test.py
+++ test.py
@@ -36,27 +36,37 @@ for i, data in enumerate(dataset):
     else:
         similar_img = Variable(data['similar_frame'])
 
-    
     if opt.no_ani:
-        
         generated = model.inference(Variable(data['reference_frames']), Variable(data['target_lmark']), None, \
          Variable(data['target_rgb']), similar_img, Variable(data['cropped_similar_image']) )
     else:
         generated = model.inference(Variable(data['reference_frames']), Variable(data['target_lmark']), \
           Variable(data['target_ani']),  Variable(data['target_rgb']), similar_img, Variable(data['cropped_similar_image'] ))
+    if not opt.use_lstm:
+        visuals = OrderedDict([( 'reference1', util.tensor2im(data['reference_frames'][0, 0,:3])),
+                                    ('target_lmark', util.tensor2im(data['target_lmark'][0])),
+                                    ('target_ani', util.tensor2im(data['target_ani'][0])),
+                                    ('synthesized_image', util.tensor2im(generated[0].data[0])),
+                                    ('masked_similar_img', util.tensor2im(generated[1].data[0])),
+                                    ('face_foreground', util.tensor2im(generated[2].data[0])),
+                                    ('beta', util.tensor2im(generated[3].data[0])),
+                                    ('alpha', util.tensor2im(generated[4].data[0])),
+                                    ('I_hat', util.tensor2im(generated[5].data[0])),
+                                    ('real_image', util.tensor2im(data['target_rgb'][0]))])
     
-    visuals = OrderedDict([( 'reference1', util.tensor2im(data['reference_frames'][0, 0,:3])),
-                                ('target_lmark', util.tensor2im(data['target_lmark'][0])),
-                                ('target_ani', util.tensor2im(data['target_ani'][0])),
-                                ('synthesized_image', util.tensor2im(generated[0].data[0])),
-                                ('masked_similar_img', util.tensor2im(generated[1].data[0])),
-                                ('face_foreground', util.tensor2im(generated[2].data[0])),
-                                ('beta', util.tensor2im(generated[3].data[0])),
-                                ('alpha', util.tensor2im(generated[4].data[0])),
-                                ('I_hat', util.tensor2im(generated[5].data[0])),
-                                ('real_image', util.tensor2im(data['target_rgb'][0]))])
-    
-    
+    else:
+        tmp = [( 'reference1', util.tensor2im(data['reference_frames'][0, 0,:3]))]
+        for t in range(opt.lstm_length):
+            tmp.append(('target_lmark_%d'%t, util.tensor2im(data['target_lmark'][0,t])))
+            tmp.append(('target_ani_%d'%t, util.tensor2im(data['target_ani'][0,t])))
+            tmp.append(('synthesized_image_%d'%t, util.tensor2im(generated[0].data[0,t])))
+            tmp.append(('masked_similar_img_%d'%t, util.tensor2im(generated[1].data[0,t])))
+            tmp.append(('face_foreground_%d'%t, util.tensor2im(generated[2].data[0,t])))
+            tmp.append(('beta_%d'%t, util.tensor2im(generated[3].data[0,t])))
+            tmp.append(('alpha_%d'%t, util.tensor2im(generated[4].data[0,t])))
+            tmp.append(('I_hat_%d'%t, util.tensor2im(generated[5].data[0,t])))
+            tmp.append(('real_image_%d'%t, util.tensor2im(data['target_rgb'][0,t])))
+        visuals = OrderedDict(tmp)
     img_path = data['v_id']
     print('process image... %s' % img_path)
 
